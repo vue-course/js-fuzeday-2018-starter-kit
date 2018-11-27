61af598840455431bcd08d831df41d5abd68f403
erezcarmel <erezcarmel@gmail.com>
2018-11-27 12:52:05 +0200

    Added cart page

---
+++ src/views/Cart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cart">
+    <ul class="items">
+      <li v-for="item in items">
+        <cart-item v-bind:data="item" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CartItem from '@/components/CartItem.vue'
+
+export default {
+    name: 'Cart',
+	computed: {
+		items() {
+			return this.$store.state.cart;
+		}
+	},
+    components: {
+      CartItem
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .cart {
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+  }
+</style>
