1c2cae70248e4db13a7efcf4b6b2983c091d52fd
Alon Sapir <>
2018-11-27 12:31:31 +0200

    saved products to vuex

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,17 +1,5 @@
 <template>
     <div class="hello">
-        <!--<h1>{{ msg }}</h1>-->
-        <!--<h3>Installed CLI Plugins</h3>-->
-        <!--<ul>-->
-            <!--<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank"-->
-                   <!--rel="noopener">unit-jest</a></li>-->
-        <!--</ul>-->
-        <!--<h3>Essential Links</h3>-->
-        <!--<ul>-->
-            <!--<li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>-->
-            <!--<li><a href="https://www.npmjs.com/package/shopify-buy" target="_blank" rel="noopener">Shopify SDK</a></li>-->
-
-        <!--</ul>-->
         <h3>
             Products
             <ul>
@@ -20,15 +8,6 @@
                 </li>
             </ul>
         </h3>
-        <!--<h3>Ecosystem</h3>-->
-        <!--<ul>-->
-            <!--<li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>-->
-            <!--<li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>-->
-            <!--<li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"-->
-                   <!--rel="noopener">vue-devtools</a></li>-->
-            <!--<li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>-->
-            <!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>-->
-        <!--</ul>-->
     </div>
 </template>
 
@@ -37,22 +16,20 @@
     import Product from "./product-details";
 
     export default {
-        name: 'HelloWorld',
+        name: 'ProductsList',
         components: {'product-details': Product},
         props: {
             msg: String
         },
-        data: function () {
-            return {
-                products: []
-            };
+        computed: {
+            products() {
+                return this.$store.getters.getProducts;
+            }
         },
         mounted: function () {
             client.product.fetchAll()
                 .then((products) => {
-                    // Do something with the products
-                    console.log(products);
-                    this.products = products;
+                    this.$store.commit('setProducts', products);
                 });
         }
     }
